29bfbd76c32e074f4d1cda69b7864c4ce65798b5
M. Mustakim Surya <muhammadmustakimsurya@gmail.com>
2024-10-10 18:00:17 +0700

    renaming file

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import authRoutes from './routes/authRoutes.js';
-import postRoutes from './routes/postRoutes.js';
 import dotenv from 'dotenv';
 import connectMongoDB from './db/connectMongoDB.js';
 import cookieParser from 'cookie-parser';
-import userRoutes from './routes/userRoutes.js';
+import authRoutes from './routes/authRoute.js';
+import postRoutes from './routes/postRoute.js';
+import userRoutes from './routes/userRoute.js';
 import {v2 as cloudinary} from 'cloudinary';
 
 dotenv.config();
