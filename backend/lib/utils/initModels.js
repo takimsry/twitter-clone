65abe59cf97264511d3ee5e90fcb53df57fd20f1
M. Mustakim Surya <muhammadmustakimsurya@gmail.com>
2024-10-25 14:50:37 +0700

    model alias added

--- backend/lib/utils/initModels.js
+++ backend/lib/utils/initModels.js
@@ -6,16 +6,16 @@ import Comment from '../../models/commentModel.js';
 import Notification from '../../models/notificationModel.js';
 
 export const initModels = () => {
-  User.hasMany(Follow, { foreignKey: 'from_user_id' });
-  User.hasMany(Follow, { foreignKey: 'to_user_id' });
+  User.hasMany(Follow, { foreignKey: 'to_user_id', as: 'Followers' });
+  User.hasMany(Follow, { foreignKey: 'from_user_id', as: 'Following' });
   User.hasMany(LikedPost, { foreignKey: 'user_id' });
   User.hasMany(Post, { foreignKey: 'user_id' });
   User.hasMany(Comment, { foreignKey: 'user_id' });
-  User.hasMany(Notification, { foreignKey: 'from_user_id' });
-  User.hasMany(Notification, { foreignKey: 'to_user_id' });
+  User.hasMany(Notification, { foreignKey: 'from_user_id', as: 'FromUser' });
+  User.hasMany(Notification, { foreignKey: 'to_user_id', as: 'ToUser' });
 
-  Follow.belongsTo(User, { foreignKey: 'from_user_id' });
-  Follow.belongsTo(User, { foreignKey: 'to_user_id' });
+  Follow.belongsTo(User, { foreignKey: 'to_user_id', as: 'Followers' });
+  Follow.belongsTo(User, { foreignKey: 'from_user_id', as: 'Following' });
   
   LikedPost.belongsTo(User, { foreignKey: 'user_id' });
   LikedPost.belongsTo(Post, { foreignKey: 'post_id' });
@@ -27,6 +27,6 @@ export const initModels = () => {
   Comment.belongsTo(Post, { foreignKey: 'post_id' });
   Comment.belongsTo(User, { foreignKey: 'user_id' });
 
-  Notification.belongsTo(User, { foreignKey: 'from_user_id' });
-  Notification.belongsTo(User, { foreignKey: 'to_user_id' });
+  Notification.belongsTo(User, { foreignKey: 'from_user_id', as: 'FromUser' });
+  Notification.belongsTo(User, { foreignKey: 'to_user_id', as: 'ToUser' });
 };
